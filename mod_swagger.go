80f95327569caa51e4267312e58b2764d9130d4b
wangfangming <wangfangming@airia.cn>
2023-07-06 10:02:27 +0800

    add swagger

--- mod_swagger.go
+++ mod_swagger.go
@@ -10,7 +10,9 @@ func WithModuleSwagger() func(wg *sync.WaitGroup) {
 	return func(wg *sync.WaitGroup) {
 		wg.Done()
 		r := Group("/docs")
-		GET(r, "/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+		GET(r, "/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler), ginSwagger.Config{
+			Title: "EZGO 接口平台",
+		})
 		Info(nil, M, "Load swagger Done!")
 	}
 }
