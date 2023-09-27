4f7953c736e5e9d209f9646d33024f4fb8304ca1
wfm <wfm@airia.cn>
2023-09-27 13:12:06 +0800

    AUTO-MSG

--- simplechat/gs_codec.go
+++ simplechat/gs_codec.go
@@ -1,16 +1,18 @@
 package simplechat
 
+import "github.com/panjf2000/gnet"
+
 type GSFrame struct {
-	StartTag    string
-	SendSeq     int  // inc
-	RecvSeq     int  // inc
-	SessionFlag byte // 0x00 request 0x01 response
+	StartTag    string // EB90
+	SendSeq     int    // inc
+	RecvSeq     int    // inc
+	SessionFlag byte   // 0x00 request 0x01 response
 	Length      int
 	Data        string
-	EndTag      string
+	EndTag      string //EB90
 }
 
-func (f *GSFrame) Encode() {
+func (f *GSFrame) Encode(c gnet.Conn) {
 
 }
 
