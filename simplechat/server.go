efe241fc3f83cc5544cf140da7cff1cb655be544
wfm <wfm@airia.cn>
2023-10-08 16:12:06 +0800

    AUTO-MSG

--- simplechat/server.go
+++ simplechat/server.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/mitchellh/mapstructure"
 	"github.com/panjf2000/gnet/v2"
+	"github.com/panjf2000/gnet/v2/pkg/buffer/ring"
 	"github.com/taerc/ezgo"
 )
 
@@ -15,11 +16,13 @@ import (
 
 type chatServer struct {
 	*gnet.BuiltinEventEngine
-	ezid *ezgo.EZID
+	ezid       *ezgo.EZID
+	readBuff   []byte
+	ringBuffer *ring.Buffer
 }
 
 func (es *chatServer) OnBoot(eng gnet.Engine) (action gnet.Action) {
-	fmt.Println("onBoot\n")
+	fmt.Println("onBoot")
 	// log.Printf("Echo server is listening on %s (multi-cores: %t, loops: %d)\n",
 	// 	srv.Addr.String(), srv.Multicore, srv.NumEventLoop)
 	return
@@ -58,11 +61,25 @@ func (es *chatServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
 }
 
 func (cs *chatServer) OnTraffic(conn gnet.Conn) (action gnet.Action) {
+	for {
+		n, e := conn.Read(cs.readBuff)
+		fmt.Println("read :", n, e)
+		fmt.Printf("tag:%02x %02x\n", cs.readBuff[0], cs.readBuff[1])
+		if n == 0 {
+			break
+		}
+		n, e = cs.ringBuffer.Write(cs.readBuff)
+		fmt.Println("ring write :", n, e)
+	}
 	n, e := conn.Write([]byte("hello traffic"))
 	fmt.Println("write: ", n, e)
 	action = -1
 	return
 }
+func (cs *chatServer) decode() {
+
+}
+
 func (es *chatServer) handlerMessage(cmd *Command, c gnet.Conn) error {
 
 	if cmd.Cmd == CommandLogin {
@@ -131,8 +148,10 @@ func (es *chatServer) commandSend(cmd *Command, c gnet.Conn) error {
 
 func StartChatServer(port int) error {
 	echo := &chatServer{
-		ezid: ezgo.NewEZID(0, 0, ezgo.ChatIDSetting()),
+		ezid:       ezgo.NewEZID(0, 0, ezgo.ChatIDSetting()),
+		readBuff:   make([]byte, 1024),
+		ringBuffer: ring.New(ring.DefaultBufferSize),
 	}
-	log.Fatal(gnet.Run(echo, fmt.Sprintf("tcp://:%d", port), gnet.WithMulticore(false)))
+	log.Fatal(gnet.Run(echo, fmt.Sprintf("tcp://:%d", port), gnet.WithMulticore(false), gnet.WithReusePort(true)))
 	return nil
 }
