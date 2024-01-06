ab7d8f585c0d44a9707c20f66dd95d94774e8305
wfm <wfm@airia.cn>
2024-01-06 19:12:05 +0800

    AUTO-MSG

--- cmd/sqlmonitor/main.go
+++ cmd/sqlmonitor/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 
@@ -19,6 +20,20 @@ func init() {
 	flag.Parse()
 }
 
+type tables struct {
+	TableName  string       `sql:"TABLE_NAME"`
+	TableType  string       `sql:"TABLE_TYPE"`
+	CreateTime int64        `sql:"CREATE_TIME"`
+	UpdateTime sql.NullTime `sql:"UPDATE_TIME"`
+}
+
+type columns struct {
+	TableName     string         `sql:"TABLE_NAME"`
+	ColumnName    string         `sql:"COLUMN_NAME"`
+	ColumnDefault sql.NullString `sql:"COLUMN_DEFAULT"`
+	ColumnComment sql.NullString `sql:"COLMN_COMMENT"`
+}
+
 func main() {
 
 	conf.LoadConfigure(ConfigPath)
@@ -31,37 +46,48 @@ func main() {
 
 }
 
+func queryTables(ctx context.Context, schema string) ([]tables, error) {
+	rows, e := ent.DB.QueryContext(ctx, "select TABLE_NAME, TABLE_TYPE, CREATE_TIME, UPDATE_TIME FROM TABLES WHERE TABLE_SCHEMA=? ", schema)
+
+	totalTables := make([]tables, 0)
+
+	if e != nil {
+		return nil, e
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+
+	}
+
+	return totalTables, nil
+}
+
 func test() {
 	ctx := context.Background()
-	rows, e := ent.DB.QueryContext(ctx, "select TABLE_NAME, COLUMN_NAME FROM COLUMNS WHERE TABLE_SCHEMA=? ", "pd_jibei")
 
+	rows, e := ent.DB.QueryContext(ctx, "select TABLE_NAME, COLUMN_NAME, COLUMN_DEFAULT, COLUMN_COMMENT FROM COLUMNS WHERE TABLE_SCHEMA=? ", "pd_jibei")
 	if e != nil {
 		fmt.Println(e.Error())
 		return
 	}
+
 	defer rows.Close()
 
-	type Columns struct {
-		TableName     string `sql:"TABLE_NAME"`
-		ColumnName    string `sql:"COLUMN_NAME"`
-		ColumnDefault string `sql:"COLUMN_DEFAULT"`
-		ColumnComment string `sql:"COLMN_COMMENT"`
-	}
-
-	provinces := make([]Columns, 0)
+	cols := make([]columns, 0)
 
 	for rows.Next() {
 
-		var p Columns
-		if se := rows.Scan(&p.TableName, &p.ColumnName); se != nil {
+		var col columns
+		if se := rows.Scan(&col.TableName, &col.ColumnName, &col.ColumnDefault, &col.ColumnComment); se != nil {
 			fmt.Println(se.Error())
 			continue
 		} else {
-			provinces = append(provinces, p)
+			cols = append(cols, col)
 		}
 	}
-	for _, v := range provinces {
-		fmt.Println(v.TableName, v.ColumnName)
+	for _, v := range cols {
+		fmt.Println(v.TableName, v.ColumnName, v.ColumnComment.String, v.ColumnDefault)
 	}
 
 }
