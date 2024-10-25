5e68374b37e99e37b7f5c6d8c20a10627df3359a
wfm-leno <wfm@leno.com>
2024-10-25 10:24:01 +0800

    upload by haier

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 	"time"
 )
 
@@ -14,7 +16,7 @@ func Work() {
 	}
 }
 
-func main() {
+func main_() {
 
 	i := 1
 	for {
@@ -25,4 +27,39 @@ func main() {
 		time.Sleep(3)
 	}
 
+}
+
+func monitorGoroutines(interval time.Duration) {
+	var initialGoroutines int
+	initialGoroutines = runtime.NumGoroutine()
+	fmt.Printf("Initial goroutines: %d\n", initialGoroutines)
+
+	ticker := time.NewTicker(interval)
+	for ; true; <-ticker.C {
+		currentGoroutines := runtime.NumGoroutine()
+		fmt.Printf("Current goroutines: %d\n", currentGoroutines)
+		if currentGoroutines-initialGoroutines >= 50 {
+			fmt.Println("Warning: High number of goroutines")
+		}
+	}
+}
+
+func createGoroutines(wg *sync.WaitGroup, num int) {
+	for i := 0; i < num; i++ {
+		go func() {
+			// 模拟工作负载
+			time.Sleep(10 * time.Millisecond)
+			wg.Done()
+		}()
+	}
+}
+
+func main() {
+	// 启动goroutine监控，每隔1秒打印一次当前goroutine数量
+	go monitorGoroutines(1 * time.Second)
+
+	var wg sync.WaitGroup
+	// 创建500个goroutine
+	createGoroutines(&wg, 500)
+	wg.Wait() // 等待所有goroutine完成工作
 }
